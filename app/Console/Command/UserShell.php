82928c4632eb83605e914cdfd485fd8f25a6480d
Eric Wettstein <eric@kudzudigitalcoachng.com>
2013-02-18 11:55:13 -0600

    Added password change function.

--- app/Console/Command/UserShell.php
+++ app/Console/Command/UserShell.php
@@ -1,10 +1,6 @@
 <?php class UserShell extends AppShell {
 	public $uses = array ('User');
 
-	public function main() {
-		$this->out("testing");
-	}
-
 	public function add() {
 		$username = $this->args[0];
 		$password = $this->args[1];
@@ -16,5 +12,22 @@
 			$this->out("could not create user");
 		}
 		
+	}
+
+	public function password() {
+		if ($this->args[0] && $this->args[1]) {
+			$user = $this->User->findByUsername($this->args[0]);
+			if ($user) {
+				$this->User->create($user);
+				$this->User->save(array('password', $this->args[1]));
+				$this->out("user updated");
+			} else { 
+				$this->out("user not found");
+			}
+			
+
+		} else {
+			$this->out("not enough parameters");
+		}
 	}
 }
